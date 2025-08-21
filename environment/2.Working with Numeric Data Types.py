c3c3623642e9d6da2a9e0a0f99808288dde72c73
Polelo Ramokhele <120188567+Reginald9999@users.noreply.github.com>
2025-08-21 15:13:00 +0200

    Add files via upload

---
+++ environment/2.Working with Numeric Data Types.py
@@ -0,0 +1,40 @@
+print("Python has three numeric types: int, float, and complex")
+
+# Exercise 2: int data type
+myVAlue1 = 1
+print(1)
+print(type(1))
+print(str(1) + " is of the data type " + str(type(1)))
+
+# Exercise 3: float data type
+myValue2 = 3.14
+print(3.14)
+print(type(3.14))
+print(str(3.14) + " is of the data type " + str(type(3.14)))
+
+# Exercise 4: complex data type
+myValue=5j
+print(5j)
+print(type(5j))
+print(str(5j) + " is of the data type " + str(type(5j)))
+
+# Exercise 5: bool data type
+myValue=True
+print(True)
+print(type(True))
+print(str(True) + " is of the data type " + str(type(True)))
+
+myValue=False
+print(False)
+print(type(False))
+print(str(False) + " is of the data type " + str(type(False)))
+
+
+
+
+
+
+
+
+
+
